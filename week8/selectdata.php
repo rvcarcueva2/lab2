7e86e11e5b894b4eedad145992b0007660fc818e
Reycel John Emmanuel Carcueva <152566933+rvcarcueva2@users.noreply.github.com>
2024-02-06 10:19:27 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " <br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " <br>";
   }
 } else {
   echo "0 results";
