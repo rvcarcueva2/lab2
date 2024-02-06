f7ec6d028ab0bdc85c20b55b62042e20dc29d48a
Reycel John Emmanuel Carcueva <152566933+rvcarcueva2@users.noreply.github.com>
2024-02-06 10:17:16 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, email, website, gender FROM rvcarcueva2_myguests";
+$sql = "SELECT id, name, email, website, gender FROM rvcarcueva2_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
